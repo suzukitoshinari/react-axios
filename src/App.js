bb207c818109e5ca1fa38dbb712839c7a5853971
suzukitoshinari <ay658mymt@gmail.com>
2021-01-25 18:21:25 +0900

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [boke, setBoke] = useState('')
+  const [punchline, setPunchline] = useState('')
+
+  const getBoke = () => {
+    axios.get('https://official-joke-api.appspot.com/jokes/random').then( 
+      (res) => {
+        setBoke("ボケ：" + res.data.setup);
+      }
+    );
+  }
+
+  const getPunchline = () => {
+    axios.get('https://official-joke-api.appspot.com/jokes/random').then( 
+      (res) => {
+        setPunchline("ツッコミ：" + res.data.punchline);
+      }
+    );
+  }
+
+  const Joke = () => {
+    getBoke();
+    getPunchline();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="App">
+        REACT-AXIOS
+      </h1>
+      <div className={"btn"} >
+        <button type="button" onClick={Joke}>Get Joke API</button>
+      </div>
+      <div className={"boke"}>
+        {boke} 
+      </div>
+      <div className={"punchline"}>
+        {punchline} 
+      </div>
     </div>
   );
 }
